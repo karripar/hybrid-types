cb73235f480811202fc246b3e2e2db73c40578e2
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-02-19 23:43:21 +0200

    update

--- DBTypes.ts
+++ DBTypes.ts
@@ -1,12 +1,12 @@
 type UserLevel = {
-  // level_id: number; // REST API
-  level_id: string; // GraphQL
+  level_id: number; // REST API
+  // level_id: string; // GraphQL
   level_name: 'Admin' | 'User' | 'Guest';
 };
 
 type User = {
-  // user_id: number; // REST API
-  user_id: string; // GraphQL
+  user_id: number; // REST API
+  // user_id: string; // GraphQL
   username: string;
   password: string;
   email: string;
@@ -15,10 +15,10 @@ type User = {
 };
 
 type MediaItem = {
-  // media_id: number; // REST API
-  // user_id: number; // REST API
-  media_id: string; // GraphQL
-  user_id: string; // GraphQL
+  media_id: number; // REST API
+  user_id: number; // REST API
+  // media_id: string; // GraphQL
+  // user_id: string; // GraphQL
   filename: string;
   thumbnail: string;
   filesize: number;
@@ -29,47 +29,47 @@ type MediaItem = {
 };
 
 type Comment = {
-  // comment_id: number; // REST API
-  // media_id: number; // REST API
-  comment_id: string; // GraphQL
-  media_id: string; // GraphQL
+  comment_id: number; // REST API
+  media_id: number; // REST API
+  // comment_id: string; // GraphQL
+  // media_id: string; // GraphQL
   user_id: number;
   comment_text: string;
   created_at: Date;
 };
 
 type Like = {
-  // like_id: number; // REST API
-  // media_id: number; // REST API
-  // user_id: number; // REST API
-  like_id: string; // GraphQL
-  media_id: string; // GraphQL
-  user_id: string; // GraphQL
+  like_id: number; // REST API
+  media_id: number; // REST API
+  user_id: number; // REST API
+  // like_id: string; // GraphQL
+  // media_id: string; // GraphQL
+  // user_id: string; // GraphQL
   created_at: Date;
 };
 
 type Rating = {
-  // rating_id: number; // REST API
-  // media_id: number; // REST API
-  // user_id: number; // REST API
-  rating_id: string; // GraphQL
-  media_id: string; // GraphQL
-  user_id: string; // GraphQL
+  rating_id: number; // REST API
+  media_id: number; // REST API
+  user_id: number; // REST API
+  // rating_id: string; // GraphQL
+  // media_id: string; // GraphQL
+  // user_id: string; // GraphQL
   rating_value: number;
   created_at: Date;
 };
 
 type Tag = {
-  // tag_id: number; // REST API
-  tag_id: string; // GraphQL
+  tag_id: number; // REST API
+  // tag_id: string; // GraphQL
   tag_name: string;
 };
 
 type MediaItemTag = {
-  // media_id: number; // REST API
-  // tag_id: number; // REST API
-  media_id: string; // GraphQL
-  tag_id: string; // GraphQL
+  media_id: number; // REST API
+  tag_id: number; // REST API
+  // media_id: string; // GraphQL
+  // tag_id: string; // GraphQL
 };
 
 type TagResult = MediaItemTag & Tag;
@@ -104,18 +104,18 @@ type UserWithNoPassword = Omit<UserWithLevel, 'password'>;
 type TokenContent = Pick<User, 'user_id'> & Pick<UserLevel, 'level_name'>;
 
 // for REST API
-// type MediaItemWithOwner = MediaItem & Pick<User, 'username'>;
+type MediaItemWithOwner = MediaItem & Pick<User, 'username'>;
 
 // FOR GRAPHQL
-type MediaItemWithOwner = MediaItem & {
-  owner: User;
-  tags?: Tag[];
-  likes?: Like[];
-  ratings?: Rating[];
-  likes_count: number;
-  average_rating?: number;
-  comments_count: number;
-};
+// type MediaItemWithOwner = MediaItem & {
+//   owner: User;
+//   tags?: Tag[];
+//   likes?: Like[];
+//   ratings?: Rating[];
+//   likes_count: number;
+//   average_rating?: number;
+//   comments_count: number;
+// };
 
 // for upload server
 type FileInfo = {
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # hybrid-types
 
-Shared typescript types for Hybridisovellukset course
+Shared typescript types for Hybridisovellukset course.
+Comment/Uncomment lines `in DBTypes.ts` for Rest API/GraphQL API
